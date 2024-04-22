925b0f2e6eb4098a710678860916f31150eee902
Darrryaa <159245758+Darrryaa@users.noreply.github.com>
2024-04-22 11:09:25 +0700

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,10 +1,4 @@
-a = int(input('Введите конец интервала:'))
-result = 0
-for i in range(2, a+1):
-    summa = 0
-    for j in range(1, i // 2 + i % 2 + 1):
-        if i % j == 0:
-            summa += j
-    if summa == i:
-        result += 1
-print(result)
+for m in range(100000, 1000000):
+    if str(m) == str(m)[::-1] and str(m % 100000) == str(m % 100000)[::-1] and str(m % 10000 // 10) == str(m % 10000 // 10)[::-1]:
+        print("Пробег автомобиля:", m)
+        break
