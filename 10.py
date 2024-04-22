1cb9c73c96e760534c92ce90babb1f73482b4c20
Darrryaa <159245758+Darrryaa@users.noreply.github.com>
2024-04-22 11:12:29 +0700

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,9 +1,10 @@
-a = int(input('Введите верхнюю границу диапазона:'))
-result = 0
-for i in range(2, a+1):
-    summa = 0
-    for j in range(1, i // 2 + i % 2 + 1):
-        if i % j == 0:
-            summa += j
-    if summa == i:
-        print(i)
+n = int(input("Введите количество кубиков N: "))
+
+p = [0] * (n + 1)
+p[0] = 1
+
+for i in range(1, n + 1):
+    for j in range(i):
+        p[i] += p[j]
+
+print(p[n])
