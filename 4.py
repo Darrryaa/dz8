afc50ac9f73303569d81c4efab3c0923dbbeb407
Darrryaa <159245758+Darrryaa@users.noreply.github.com>
2024-04-22 11:09:03 +0700

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,6 +1,10 @@
-x = int(input('Введите целое число:'))
-summa = 0
-for i in range(x+1):
-    summa += i
-    if summa != 0:
-        print(summa)
+N = int(input('Введите число веревок,выданное команде:'))
+k=0
+while N>0:
+    N = int(input('Введите число веревок,выданное команде:'))
+    if N==0:
+         break
+    else:
+        if N%4==0:
+            k+=1
+print(k)
